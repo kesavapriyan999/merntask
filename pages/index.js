9c3ba61fc1309e53a343f8bafdcfe806dc3555eb
kesavapriyan999 <106240065+kesavapriyan999@users.noreply.github.com>
2022-05-25 17:09:03 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -88,7 +88,7 @@ export default function Home() {
       <h1 className="text-sm text-bold mt-10 mob:mt-2 laptop:mt-10 mob:p-2 laptop:p-0">
         Made With ❤ by{" "}
         <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+          <a className="underline underline-offset-1">kesavapriyan</a>
         </Link>
       </h1>
     </div>
